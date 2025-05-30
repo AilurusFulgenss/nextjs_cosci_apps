c0a296a2c7b532bb9e819b2928dfe620f67621d1
AilurusFulgenss <kudatonnam@gmail.com>
2025-05-30 14:36:47 +0700

    all

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email("รูปแบบอีเมลไม่ถูกต้อง"),
+  password: z.string().min(6, "รหัสผ่านต้องอย่างน้อย 6 ตัวอักษร"),
+});
+
+const Login01Page = () => {
+  const router = useRouter();
+  
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = async (form: z.infer<typeof formSchema>) => {
+          await authClient.signIn.email({
+                  email: form.email,
+                  password: form.password,
+              }, {
+                  onRequest: (ctx) => {
+                    //show loading
+                    console.log(ctx.body);
+                  },
+                  onSuccess: (ctx) => {
+                    //redirect to the dashboard or sign in page
+                    console.log(ctx.data);
+                    router.replace('/dashboard');
+                  },
+                  onError: (ctx) => {
+                      // display the error message
+                      alert(ctx.error.message);
+                  },
+          });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs w-full flex flex-col items-center">
+        
+        <p className="mt-4 text-xl font-bold tracking-tight">
+          เข้าสู่ระบบขายสินค้าออนไลน์ CoSci
+        </p>
+
+        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
+          <Separator />
+        </div>
+
+        <Form {...form}>
+          <form
+            className="w-full space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-4 w-full">
+              Log In
+            </Button>
+          </form>
+        </Form>
+
+        <div className="mt-5 space-y-5">
+          <Link
+            href="#"
+            className="text-sm block underline text-muted-foreground text-center"
+          >
+            Forgot your password?
+          </Link>
+          <p className="text-sm text-center">
+            Don&apos;t have an account?
+            <Link href="/signup" className="ml-1 underline text-muted-foreground">
+              Create account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Login01Page;
---
+++ src/components/navbar-01/navbar-01.tsx
@@ -0,0 +1,69 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "./logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+import Link from "next/link";
+import { ShoppingBasket } from "lucide-react";
+import { Badge } from "../ui/badge";
+import CountCartItem from "@/app/(front)/components/CountCartItem";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+const Navbar01Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  return (
+    <div className="bg-muted">
+      <nav className="h-16 bg-background border-b">
+        <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Logo />
+
+          {/* Desktop Menu */}
+          <NavMenu className="hidden md:block" />
+
+          <div className="flex items-center gap-3">
+
+          <Link href="/cart">
+            <Badge className="p-2 text-sm"><ShoppingBasket /> <CountCartItem /> item (s)</Badge>
+          </Link>
+
+          {
+            !session && (
+            <>
+              <Button asChild variant="outline" className="hidden sm:inline-flex">
+                <Link href="/login">เข้าสู่ระบบ</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/signup">สมัครสมาชิก</Link>
+              </Button> 
+            </>
+            )
+          }
+
+          {
+            session && (
+              <>
+                <div className="flex items-center">
+                  Hello, {session.user.name}
+                </div>
+                <Button variant="destructive" asChild>
+                  <Link href="/dashboard">Dashboard</Link>
+                </Button>
+              </>
+            )
+          }
+
+            {/* Mobile Menu */}
+            <div className="md:hidden">
+              <NavigationSheet />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar01Page;
